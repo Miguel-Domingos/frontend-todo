59ff246e630926a867c0d7ae71e45c1cbaea6187
Miguel Domingos <mighueldomingos@gmail.com>
2024-07-16 14:03:01 +0200

    fix: :bug: .env backend api variable name

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-const { BACKEND_API_URL } = import.meta.env;
+const { VITE_API_URL } = import.meta.env;
 
 export const AxiosConfig = axios.create({
-  baseURL: BACKEND_API_URL,
+  baseURL: VITE_API_URL,
 });
